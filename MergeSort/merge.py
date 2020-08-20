9affb3dc9f7df2079b1af8f049b78b33b5877779
João Victor <joaosantoos@aol.com>
2020-08-20 18:57:57 -0300

    Update merge.py

--- MergeSort/merge.py
+++ MergeSort/merge.py
@@ -38,3 +38,39 @@ print('The sorted given list: '+ str(mergesort(a)))
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+'''
+def recursiveinsertion(arr, n):
+    if n <= 1:
+        return
+
+    recursiveinsertion(arr, n-1)
+
+    last = arr[n-1]
+    j = n-2
+
+    while (j >= 0 and arr[j] > last):
+        arr[j+1] = arr[j]
+        j = j-1
+
+    arr[j+1] = last
+
+def printarr(arr, n):
+    print ('The sorted given list: '+ str(arr))
+
+arr = [90,7,85,13,71,22,67,36,59,45]
+n = len(arr)
+recursiveinsertion(arr, n)
+print(printarr(arr, n))
+'''
+
