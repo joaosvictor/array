67c7e8372b5a52e917f24c136a3f9eb5722f39b5
João Victor <joaosantoos@aol.com>
2020-08-20 18:48:54 -0300

    merge it

---
+++ MergeSort/merge.py
@@ -0,0 +1,40 @@
+# time complexity: O(1), O(2), O(n log n)   
+# https://en.wikipedia.org/wiki/Merge_sort
+
+def mergesort(a):
+  if len(a) == 1:
+    return a
+  elif len(a) == 2:
+    if a[0] > a[1]:
+      return [a[1], a[0]]
+    else:
+      return a
+
+  p = len(a)//2
+  m1 = mergesort(a[:p])
+  m2 = mergesort(a[p:])
+
+  ret = []
+  while 1:
+    if len(m1) > 0 and len(m2) > 0:
+      if m1[0] <= m2[0]:
+        ret.append(m1[0])
+        m1 = m1[1:]
+      else:
+        ret.append(m2[0])
+        m2 = m2[1:]
+    elif len(m1) > 0:
+      ret += m1
+      m1 = []
+    elif len(m2) > 0:
+      ret += m2
+      m2 = []
+    else:
+      break
+  return ret
+
+a = [90,7,85,13,71,22,67,36,59,45]
+print('The sorted given list: '+ str(mergesort(a)))
+
+
+
