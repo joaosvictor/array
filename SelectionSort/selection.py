1c2372ca431361640f7408820cdd468281153d70
João Victor <joaosantoos@aol.com>
2020-08-20 18:54:43 -0300

    selection it

---
+++ SelectionSort/selection.py
@@ -0,0 +1,21 @@
+# time complexity: O(n^2)
+# https://en.wikipedia.org/wiki/Selection_sort    
+
+def findSmallest(arr):
+  smallest = arr[0]
+  smallest_index = 0
+
+  for i in range(1, len(arr)):
+    if arr[i] < smallest:
+      smallest_index = i
+      smallest = arr[i]      
+  return smallest_index
+
+def selectionSort(arr):
+  newArr = []
+  for i in range(len(arr)):
+      smallest = findSmallest(arr)
+      newArr.append(arr.pop(smallest))
+  return newArr
+
+print('The sorted given list: ' + str(selectionSort([90,7,85,13,71,22,67,36,59,45])))     
