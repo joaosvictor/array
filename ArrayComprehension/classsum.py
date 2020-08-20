1597c4b724867150e218080c4ce188671ecf3e0a
João Victor <joaosantoos@aol.com>
2020-08-20 18:33:56 -0300

    Create classsum.py

---
+++ ArrayComprehension/classsum.py
@@ -0,0 +1,14 @@
+# make a real manipulation on this array 
+# [90,7,85,13,71,22,67,36,59,45]
+import sys
+import os
+import re
+class classsum:
+    def sumall():
+        # this should work on a new sum
+        arr = [90,7,85,13,71,22,67,36,59,45]  
+        sm = sum(arr[0:len(arr)])
+        print('The merge sum of the first item is: '+str(sm))
+    sumall()
+print(classsum)    
+
